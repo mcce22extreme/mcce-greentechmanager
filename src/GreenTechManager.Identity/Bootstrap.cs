c7014b820eb386ab76b4a34926d6ab03a1172192
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2022-09-25 13:17:18 +0200

    Added identity service

---
+++ src/GreenTechManager.Identity/Bootstrap.cs
@@ -0,0 +1,51 @@
+﻿using GreenTechManager.Core;
+using GreenTechManager.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreenTechManager.Identity
+{
+    public class Bootstrap : BootstrapBase
+    {
+        protected override WebApplicationBuilder CreateWebApplicationBuilder(string[] args)
+        {
+            var builder = base.CreateWebApplicationBuilder(args);
+
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("identitydb"));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddIdentityServer(opt =>
+            {
+                opt.Events.RaiseErrorEvents = true;
+                opt.Events.RaiseInformationEvents = true;
+                opt.Events.RaiseFailureEvents = true;
+                opt.Events.RaiseSuccessEvents = true;
+                opt.EmitStaticAudienceClaim = true;
+            })
+                .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryClients(Config.Clients)
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryApiScopes(Config.Scopes)
+                .AddAspNetIdentity<ApplicationUser>();
+
+            return builder;
+        }
+
+        protected override WebApplication CreateApplication(string[] args)
+        {
+            var app = base.CreateApplication(args);
+
+            app.UseIdentityServer();
+
+            DataSeed.Seed(app);
+
+            return app;
+        }
+    }
+}
---
+++ src/GreenTechManager.Identity/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using GreenTechManager.Identity.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenTechManager.Identity.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        public AuthController()
+        {
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var nvc = new List<KeyValuePair<string, string>>();
+            nvc.Add(new KeyValuePair<string, string>("client_id", model.ClientId));
+            nvc.Add(new KeyValuePair<string, string>("grant_type", "password"));
+            nvc.Add(new KeyValuePair<string, string>("username", model.UserName));
+            nvc.Add(new KeyValuePair<string, string>("password", model.Password));
+            //nvc.Add(new KeyValuePair<string, string>("scope", string.Join(' ', model.Scopes)));
+            
+            var request = new HttpRequestMessage(HttpMethod.Post, $"http://{HttpContext.Request.Host}/connect/token")
+            {
+                Content = new FormUrlEncodedContent(nvc)
+            };
+            var response = await HttpClient.SendAsync(request);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok(content);
+            }
+            else
+            {
+                return Unauthorized(content);
+            }
+        }
+    }
+}
