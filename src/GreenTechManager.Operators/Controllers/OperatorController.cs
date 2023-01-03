77fbf6d038bff739e78a27667732c67d5823ea39
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2023-01-03 13:10:26 +0100

    Added operation description for operator service.

--- src/GreenTechManager.Operators/Controllers/OperatorController.cs
+++ src/GreenTechManager.Operators/Controllers/OperatorController.cs
@@ -19,18 +19,42 @@ namespace GreenTechManager.Operators.Controllers
             _operatorManager = operatorManager;
         }
 
+        /// <summary>
+        /// Retrieve a list of available operators.
+        /// </summary>
+        /// <response code="200">Operators retrieved successfully.</response>
+        /// <response code="401">No authentication information provided.</response>
+        /// <response code="403">Not authorized to retrieve operators.</response>
         [HttpGet]
         public async Task<OperatorModel[]> GetOperators()
         {
             return await _operatorManager.GetOperators();
         }
 
+        /// <summary>
+        /// Retrieve a specific operator.
+        /// </summary>
+        /// <param name="operatorId" example="1">The operator id.</param>
+        /// <returns>The operator with the given id.</returns>
+        /// <response code="200">Operator retrieved successfully.</response>
+        /// <response code="401">No authentication information provided.</response>
+        /// <response code="403">Not authorized to retrieve operators.</response>
+        /// <response code="404">Operator with the given id was not found.</response>
         [HttpGet("{operatorId}")]
         public async Task<OperatorModel> GetOperator(int operatorId)
         {
             return await _operatorManager.GetOperator(operatorId);
         }
 
+        /// <summary>
+        /// Create a new operator.
+        /// </summary>
+        /// <param name="model">Information of the new operator.</param>
+        /// <returns>The new created operator.</returns>
+        /// <response code="200">Operator created successfully.</response>
+        /// <response code="400">Validation error occured.</response>
+        /// <response code="401">No authentication information provided.</response>
+        /// <response code="403">Not authorized to create operators.</response>
         [HttpPost]
         [Authorize(Policy = AuthConstants.RequireAdminRolePolicy)]
         public async Task<OperatorModel> CreateOperator([FromBody] SaveOperatorModel model)
@@ -38,6 +62,17 @@ namespace GreenTechManager.Operators.Controllers
             return await _operatorManager.CreateOperator(model);
         }
 
+        /// <summary>
+        /// Update an existing operator.
+        /// </summary>
+        /// <param name="operatorId" example="1">The operator id.</param>
+        /// <param name="model">Information of the operator.</param>
+        /// <returns>The updated operator.</returns>
+        /// <response code="200">Operator updated successfully.</response>
+        /// <response code="400">Validation error occured.</response>
+        /// <response code="401">No authentication information provided.</response>
+        /// <response code="403">Not authorized to update operators.</response>
+        /// <response code="404">Operator with the given id was not found.</response>
         [HttpPut("{operatorId}")]
         [Authorize(Policy = AuthConstants.RequireAdminRolePolicy)]
         public async Task<OperatorModel> UpdateOperator(int operatorId, [FromBody] SaveOperatorModel model)
@@ -45,6 +80,14 @@ namespace GreenTechManager.Operators.Controllers
             return await _operatorManager.UpdateOperator(operatorId, model);
         }
 
+        /// <summary>
+        /// Delete an existing operator.
+        /// </summary>
+        /// <param name="operatorId" example="1">The operator id.</param>
+        /// <response code="200">Operator deleted successfully.</response>        
+        /// <response code="401">No authentication information provided.</response>
+        /// <response code="403">Not authorized to delete operators.</response>
+        /// <response code="404">Operator with the given id was not found.</response>
         [HttpDelete("{operatorId}")]
         [Authorize(Policy = AuthConstants.RequireAdminRolePolicy)]
         public async Task DeleteOperator(int operatorId)
--- src/GreenTechManager.Operators/Models/OperatorModel.cs
+++ src/GreenTechManager.Operators/Models/OperatorModel.cs
@@ -6,30 +6,60 @@ namespace GreenTechManager.WindParks.Models
 {
     public class OperatorModel : AuditableModelBase
     {
+        /// <summary>
+        /// The identifier of the operator.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The name of the operator.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The address of the operator.
+        /// </summary>
         public string Address { get; set; }
 
+        /// <summary>
+        /// The city of the operator.
+        /// </summary>
         public string City { get; set; }
 
+        /// <summary>
+        /// The zip code of the operator.
+        /// </summary>
         public int Zip { get; set; }
 
+        /// <summary>
+        /// The country of the operator.
+        /// </summary>
         public string Country { get; set; }
     }
 
     public class SaveOperatorModel
     {
+        /// <summary>
+        /// The name of the operator.
+        /// </summary>
         [Required]
         public string Name { get; set; }
 
+        /// <summary>
+        /// The address of the operator.
+        /// </summary>
         [Required]
         public string Address { get; set; }
 
+        /// <summary>
+        /// The city of the operator.
+        /// </summary>
         [Required]
         public string City { get; set; }
 
+        /// <summary>
+        /// The zip code of the operator.
+        /// </summary>
         [Required]
         public int Zip { get; set; }
 
